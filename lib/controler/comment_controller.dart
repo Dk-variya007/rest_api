18a544e48e584d95a66d0cc1b67257bf9ff8b284
Dk-variya007 <variyadivyesh79@gmail.com>
2025-04-13 22:44:27 +0530

    update

--- lib/controler/comment_controller.dart
+++ lib/controler/comment_controller.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: camel_case_types
 
 import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 import 'package:rest_api/Models/comment_model.dart';
@@ -9,10 +10,28 @@ class CommentController extends GetxController {
   RxList list = [].obs;
   var isLoading = true.obs;
 
+  static bool isLoaderShow = false;
+  Rx<Error> error = Error().obs;
+
+  void showLoader() async {
+    isLoaderShow = true;
+    await Get.dialog(Center(child: CircularProgressIndicator()),
+        barrierDismissible: true);
+    isLoaderShow = false;
+  }
+
+  void dismissLoader() {
+    // if (isLoaderShow) {
+    //   Get.closeAllSnackbars();
+    Get.back(closeOverlays: false, canPop: false);
+  }
+
   void getData() async {
     try {
+      showLoader();
       final response = await http
           .get(Uri.parse("https://jsonplaceholder.typicode.com/comments"));
+      dismissLoader();
       if (response.statusCode == 200) {
         List<CommentModel> loadComment = [];
         var data = jsonDecode(response.body);
--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,6 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const UserDataFreezed());
+        home: const CommentModelScreen());
   }
 }
